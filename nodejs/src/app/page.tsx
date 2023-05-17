215f3de484502936fbec66bb8b1db6a4cab11993
ryo <fry3292@gmail.com>
2023-05-17 12:44:45 +0900

    feat: default revalidateを設定 use側のrevalidateに寄っているため

--- nodejs/src/app/page.tsx
+++ nodejs/src/app/page.tsx
@@ -3,6 +3,8 @@ import { use, useEffect, Suspense } from "react";
 
 import { Index } from "./Users/index";
 
+export const revalidate = 10;
+
 const main = () => {
   const date = new Date();
   const formattedDate = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
