51ac19960e55e4bcd8efd275a13bc916aba91161
ryo.fujimoto <ryo.fujimoto@caters.co.jp>
2023-05-18 16:21:23 +0900

    fix: 60秒待つため、ファイルごと分ける

--- nodejs/src/app/Users/date_cc.tsx
+++ nodejs/src/app/Users/date_cc.tsx
@@ -3,7 +3,7 @@
 import { use, useEffect, Suspense, memo, useState, cache } from "react";
 import { useRouter } from "next/navigation";
 
-import { getData2 as getData } from "./getData";
+import { getData2 as getData } from "./getData2";
 
 const ViewClientData = () => {
   const router = useRouter();
--- nodejs/src/app/Users/getData2.ts
+++ nodejs/src/app/Users/getData2.ts
@@ -1,12 +1,11 @@
-// 同じ関数で引数だけ変えてrevalidateを変えた場合、低い方に揃っていた。
-const getData = async () => {
+const getData2 = async () => {
   // await new Promise((resolve) => setTimeout(resolve, 5000));
 
-  const url = "https://ryo1999.com/now.php";
+  const url = "https://ryo1999.com/now2.php";
   // const url = "https://jsonplaceholder.typicode.com/users";
 
   const options = {
-    next: { revalidate: 60 },
+    next: { revalidate: 10 },
   };
 
   const res = await fetch(url, options);
@@ -16,4 +15,4 @@ const getData = async () => {
   return data;
 };
 
-export { getData };
+export { getData2 };
